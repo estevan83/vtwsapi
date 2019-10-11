0bc7a88fa106ae6829e7ec68306605fbc3c8800a
estevan <estefan.civera@gmail.com>
2019-10-11 12:16:50 +0200

    Primo commit integra

---
+++ index.php
@@ -0,0 +1,247 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$user = "testinsert@dapsides.it";
+$pass = "testinsert@dapsides.it";
+$url = "http://62.97.45.44:443/webapp/api/v3/createToken.php/process/" . $user . "/" . $pass;
+//echo $url;
+
+//$token = "dd6a6fcdc0ae1b8b463b4bb691a0f12a";
+$token = "";
+
+
+
+
+//  2.1. Inserimento di un ordine
+$insertNewOrder = true; // non funziona
+
+
+
+//2.3. Interrogazione ultimo stato
+$lastOrderStatus = false; // Da verificare con un ordine vero
+$lastOrderStatusID = 100;
+
+// 2.4. Interrogazione storico GLS
+$storicogls = false; // OK Sembra funzionare => da verifcare con un ID VERO
+
+
+// 3.1. Interrogazione Stock prodotti
+$getqtyproduct = false; // Login non autorizzata
+$codicearticolo = "1";
+
+// 4.1. Inserimento contatto
+$creaContatto = false; // ERRORE
+
+
+
+
+
+
+if(strlen($token) == 0){
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_VERBOSE, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+    if (curl_errno($ch)) {
+        echo curl_errno($ch);
+        echo curl_error($ch);
+        $status = array("status" => "error", "msg" => "Errore di Login");
+        return false;
+    } else {
+        $result = curl_exec($ch);
+        $response = json_decode($result);
+        curl_close($ch);
+        $token = $response->csrf_value;
+    }
+}
+
+// -----------------------------------------------------------
+// Creazione del contatto
+// ------------------------------------------------------------
+if($creaContatto == true){
+    
+    $url="http://62.97.45.44:443/webapp/api/index.php/insertLead";
+
+    $data = array(
+        'leadID' => '999',
+        'customerFirstName' => 'estevan',
+        'customerLastName' => 'civera',
+        'destEmail' => 'lamail@mail.it',
+        'productName' => 'nome prodotto',
+        'totalAmount' => 100,
+    );
+    
+    echo "Creazione del contatto" .PHP_EOL;
+    print_r($data);
+
+    $data = json_encode($data);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_VERBOSE, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Authorization: '.$token));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    if (curl_errno($ch)) {
+        echo curl_errno($ch);
+        echo curl_error($ch);
+    } else {
+        $result = curl_exec($ch);
+        $response = json_decode($result);
+        echo $result.PHP_EOL;
+        curl_close($ch);
+    }
+}
+
+
+if ($getqtyproduct == true) {
+
+    
+    $data = array("item" => $codicearticolo);
+    
+    $data = json_encode($data);
+
+    $url = "http://62.97.45.44:443/webapp/api/index.php/getqtyproduct";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_VERBOSE, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_POST, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: ' . $token));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    if (curl_errno($ch)) {
+        echo curl_errno($ch);
+        echo curl_error($ch);
+        $status = array("status" => "error", "msg" => "Prodotto non trovato");
+    } else {
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $response = json_decode($result);
+        $tt = array();
+        $i = 1;
+        echo $result;
+    }
+}
+
+
+
+if ($storicogls == true) {
+
+    $data = array('deliveryNote'=>$deliveryNote,"codvet"=>'GLS');
+    $data = json_encode($data);
+    
+    $url = "http://62.97.45.44:443/webapp/api/index.php/getGLSstorico";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_VERBOSE, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: ' . $token));
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    if (curl_errno($ch)) {
+        $status = array("status" => "error", "msg" => "Spedizione Non Trovata " . curl_error($ch));
+        echo json_encode($status);
+    } else {
+        $result = curl_exec($ch);
+        $response = json_decode($result, true);
+        if ($response['status'] == 'success') {
+            echo json_encode($response);
+        } else {
+            echo "Tracking non trovato";
+        }
+
+        curl_close($ch);
+    }
+}
+
+
+
+if ($lastOrderStatus == true) {
+    
+    $url = "http://62.97.45.44:443/webapp/api/index.php/getlaststatus";
+    $data = array('orderID' => $lastOrderStatusID);
+    $data = json_encode($data);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_VERBOSE, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_POST, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:  application/json', 'Authorization: ' . $token));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+    if (curl_errno($ch)) {
+        echo curl_errno($ch);
+        echo curl_error($ch);
+        $status = array("status" => "error", "msg" => "Spedizione non trovata");
+    } else {
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $response = json_decode($result);
+        echo $result;
+    }
+}
+
+
+
+if ($insertNewOrder == true) {
+
+    $url = "http://62.97.45.44:443/webapp/api/index.php/insertorder";
+
+
+    $data = '{"1":{"orderID":"XXXXX","customerName":"XXXXXX","destAddress":"XXXXX","destPostalCode":"XXXXX",'
+            . '"destCity":"XXXXXX","destAreaCode":"XX","destCountryCode":"XX","destPhone":"XXXXXXXX", '
+            . '"destEmail":"XXXXXXX","orderDate":"YYYY-MM-DD",'
+            . '"deliveryInfo":"Lorem ipsum","cod":XX,"totalAmount":XX.XX,"orderRowID":"X","productID":"XXXX","productName":"XXXXX","productQty":X}';
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_VERBOSE, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: ' . $token));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    if (curl_errno($ch)) {
+        echo curl_errno($ch);
+        echo curl_error($ch);
+    } else {
+        $result = curl_exec($ch);
+        $response = json_decode($result);
+        echo $result;
+        curl_close($ch);
+    }
+}
+
+/*
+    Nell ordine
+        contactrecord :   id di vtiger del contatto (YxZ)
+ 
+  
+    
+ */
---
+++ vtws.php
@@ -0,0 +1,159 @@
+<?php
+
+
+$createContact = false;
+$createOrder = false;
+
+$updateOrder = true;
+
+
+
+
+$usr = '';
+$ack = '';
+$url = '';
+
+
+include_once('vtwsclib/Vtiger/WSClient.php');
+
+
+$client = new Vtiger_WSClient($url);
+$login = $client->doLogin($usr, $ack);
+if(!$login) 
+    echo 'Login Failed';
+else echo 'Login Successful';
+
+
+/*
+ * [idPrefix] => 
+ * 11 Accounts
+ * 14 Produtcts
+ * 12 Contacts
+ * 6 SalesOrder
+ */
+/*
+print_r($client->doDescribe('SalesOrder'));
+
+die();
+*/
+//----------------------------------
+// Creazione del contatto in Vtiger
+//----------------------------------
+if($createContact == true){
+
+    $data = array (
+                'lastname' => 'C0gnome',
+                'firstname'=> 'Nome',
+                'company'  => 'Algoma',
+                'phone'  => '1234567890',
+               // 'assigned_user_id' => '19x7' , // 19=Users Module ID, 1=First user Entity ID
+    );
+
+    $module = 'Contacts';
+    $record = $client->doCreate($module,$data);
+    
+    if($record) {
+        $recordid = $client->getRecordId($record['id']);
+        /*
+            The server returns record['id'] in the format <moduleid>'x'<recordid>.
+            Use method getRecordId method can be used to retrieve only the record id
+            part from the returned record id from the server.
+         */
+    }
+    else{
+        $error = $client->lastError();
+        throw new Exception($error['code']. ' ' .$error['message']);
+    }
+}
+
+//----------------------------------
+// Creo ordine in vtiger
+//----------------------------------
+if($createOrder == true){
+    $data = array(
+            'subject' =>  'Ordine NUmero',
+            'sostatus' => 'Created',
+            'invoicedate' => date("Y-m-d"),
+            'invoicestatus' => 'Created',
+            'account_id' => '11x65',
+            'contact_id' => '12x107',
+           // 'assigned_user_id' => '19x1',
+            'bill_street' => 'bill_street',
+            'ship_street' =>'ship_street',
+            'bill_city' => 'bill_city',
+            'ship_city' => 'ship_city',
+            'bill_state' => 'bill_state',
+            'ship_state' => 'ship_state',
+            'bill_code' => 'bill_code',
+            'ship_code' => 'ship_code',
+            'bill_country' => 'bill_country',
+            'ship_country' => 'ship_country',
+            'bill_pobox' => 'bill_pobox',
+            'ship_pobox' => 'ship_pobox',
+			
+            'terms_conditions' =>'terms_conditions',
+            'description' => 'description',
+             
+           // 'currency_id' => vtws_getWebserviceEntityId('Currency','1'),
+            'hdnTaxType' => 'group',
+            'productid' => '14x56',
+            'hdnDiscountAmount' => '0',
+            'hdnS_H_Amount' => 0,
+            'hdnS_H_Percent' => 0,
+            'LineItems' => array(
+				
+				array(
+					'productid' => '14x56',
+					'quantity' => 10,
+					'listprice' => 59,
+					'comment' => 'testo che voglio inserire'
+				),
+				
+				array(
+					'productid' => '14x56',
+					'quantity' => 6,
+					'listprice' => 11,
+					'comment' => 'testo che voglio inserire AAAAAA'
+				),
+					
+			),
+        );
+    
+    $module = 'SalesOrder';
+    $record = $client->doCreate($module,$data);
+    
+    if($record) {
+        print_r($record);
+    }
+    else{
+        $error = $client->lastError();
+        throw new Exception($error['code']. ' ' .$error['message']);
+    }
+}
+
+/* */
+//----------------------------------
+// Aggiornamento stato ordine
+//----------------------------------
+
+//----------------------------------
+    //
+    
+    $module = 'SalesOrder';
+    $dataMM = array('sostatus' => 'Delivered','id' => '6x142' );
+    
+    
+    $data[]
+    $record = $client->doRevise($module, $data);
+    
+    if($record) {
+        print_r($record);
+    }
+    else{
+        $error = $client->lastError();
+        throw new Exception($error['code']. ' ' .$error['message']);
+    }
+    
+   
+    
+           
